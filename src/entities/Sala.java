bc94a360d0e85607abb23b8f89f5b2ade9beffb0
pecorarolucio <luciopecoraro2002@gmail>
2023-07-15 09:43:11 -0300

    agrego sala

---
+++ src/entities/Sala.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Sala {
+	private  int idSala;
+	private int capacidadMaxima;
+	
+	
+	
+	
+	
+	
+	
+	
+	public Sala(int idSala, int capacidadMaxima) {
+		super();
+		this.idSala = idSala;
+		this.capacidadMaxima = capacidadMaxima;
+	}
+	public int getIdSala() {
+		return idSala;
+	}
+	public void setIdSala(int idSala) {
+		this.idSala = idSala;
+	}
+	public int getCapacidadMaxima() {
+		return capacidadMaxima;
+	}
+	public void setCapacidadMaxima(int capacidadMaxima) {
+		this.capacidadMaxima = capacidadMaxima;
+	}
+	
+
+}
