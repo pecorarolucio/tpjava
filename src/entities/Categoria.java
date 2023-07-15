1c2e92115404648522d09e2fed809bf9517d2290
pecorarolucio <luciopecoraro2002@gmail>
2023-07-14 22:51:27 -0300

    agrego src

---
+++ src/entities/Categoria.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Categoria {
+	private int idCategoria;
+	private String nombreCategoria;
+	
+	
+	
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getNombreCategoria() {
+		return nombreCategoria;
+	}
+	public void setNombreCategoria(String nombreCategoria) {
+		this.nombreCategoria = nombreCategoria;
+	}
+	
+	
+	public Categoria(int idCategoria, String nombreCategoria) {
+		super();
+		this.idCategoria = idCategoria;
+		this.nombreCategoria = nombreCategoria;
+	}
+	
+	
+	
+	
+}
