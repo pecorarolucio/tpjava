4d6dd601821e494393b8dc884e0fb7cac0d93b41
Juan Ignacio <condorijuan95@gmail.com>
2023-11-19 17:37:26 -0300

    base actualizada

--- cinemanagement3.sql
+++ cinemanagement3.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `cinemanagement` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinemanagement`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: localhost    Database: cinemanagement3
+-- Host: 127.0.0.1    Database: cinemanagement
 -- ------------------------------------------------------
 -- Server version	8.0.33
 
@@ -23,9 +25,9 @@ DROP TABLE IF EXISTS `categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria` (
-  `idCategoria` int unsigned NOT NULL AUTO_INCREMENT,
+  `idcategoria` int unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
-  PRIMARY KEY (`idCategoria`)
+  PRIMARY KEY (`idcategoria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,15 +49,15 @@ DROP TABLE IF EXISTS `entrada`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entrada` (
-  `codentrada` int unsigned NOT NULL AUTO_INCREMENT,
+  `codentrada` int unsigned NOT NULL,
   `precio` int unsigned NOT NULL,
   `fecha` date NOT NULL,
   `HoraInicio` time NOT NULL,
   `IDSala` int unsigned NOT NULL,
   PRIMARY KEY (`codentrada`),
-  KEY `IDSala_idx` (`IDSala`),
-  CONSTRAINT `id_sala` FOREIGN KEY (`IDSala`) REFERENCES `sala` (`idSala`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `Ent_Func_idx` (`fecha`,`HoraInicio`,`IDSala`),
+  CONSTRAINT `Ent_Func` FOREIGN KEY (`fecha`, `HoraInicio`, `IDSala`) REFERENCES `funcion` (`fecha`, `HoraInicio`, `IDSala`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +66,6 @@ CREATE TABLE `entrada` (
 
 LOCK TABLES `entrada` WRITE;
 /*!40000 ALTER TABLE `entrada` DISABLE KEYS */;
-INSERT INTO `entrada` VALUES (0,1200,'2023-10-10','17:00:00',1),(1,1200,'2023-10-10','17:00:00',2);
 /*!40000 ALTER TABLE `entrada` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,8 +85,8 @@ CREATE TABLE `funcion` (
   PRIMARY KEY (`fecha`,`HoraInicio`,`IDSala`),
   KEY `IDSala_idx` (`IDSala`),
   KEY `IDpelicula_idx` (`IDPelicula`),
-  CONSTRAINT `IDpelicula` FOREIGN KEY (`IDPelicula`) REFERENCES `pelicula` (`idPelicula`),
-  CONSTRAINT `IDsala` FOREIGN KEY (`IDSala`) REFERENCES `sala` (`idSala`)
+  CONSTRAINT `IDpelicula` FOREIGN KEY (`IDPelicula`) REFERENCES `pelicula` (`idpelicula`),
+  CONSTRAINT `IDsala` FOREIGN KEY (`IDSala`) REFERENCES `sala` (`idsala`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -95,7 +96,7 @@ CREATE TABLE `funcion` (
 
 LOCK TABLES `funcion` WRITE;
 /*!40000 ALTER TABLE `funcion` DISABLE KEYS */;
-INSERT INTO `funcion` VALUES ('2023-10-10','17:00:00','20:00:00',1,2),('2023-10-10','17:00:00','19:00:00',2,3);
+INSERT INTO `funcion` VALUES ('2024-12-24','09:00:00','10:45:00',1,1),('2024-12-24','11:00:00','12:45:00',2,1);
 /*!40000 ALTER TABLE `funcion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,13 +108,13 @@ DROP TABLE IF EXISTS `pelicula`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pelicula` (
-  `idPelicula` int unsigned NOT NULL AUTO_INCREMENT,
-  `idCategoria` int unsigned NOT NULL,
+  `idpelicula` int unsigned NOT NULL AUTO_INCREMENT,
+  `idcategoria` int unsigned NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  PRIMARY KEY (`idPelicula`),
-  KEY `idcategoria_idx` (`idCategoria`),
-  CONSTRAINT `idcategoria` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idpelicula`),
+  KEY `idcategoria_idx` (`idcategoria`),
+  CONSTRAINT `idcategoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +123,7 @@ CREATE TABLE `pelicula` (
 
 LOCK TABLES `pelicula` WRITE;
 /*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
-INSERT INTO `pelicula` VALUES (1,3,'Busqueda implacable'),(2,2,'Barbie'),(3,1,'Dune');
+INSERT INTO `pelicula` VALUES (1,1,'La noche de ciencia ficcion');
 /*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,7 +143,7 @@ CREATE TABLE `reseña` (
   PRIMARY KEY (`codigo`,`IDPelicula`),
   KEY `IDPelicula_idx` (`IDPelicula`),
   KEY `nrousuario_idx` (`nrousuario`),
-  CONSTRAINT `ID_Pelicula` FOREIGN KEY (`IDPelicula`) REFERENCES `pelicula` (`idPelicula`),
+  CONSTRAINT `ID_Pelicula` FOREIGN KEY (`IDPelicula`) REFERENCES `pelicula` (`idpelicula`),
   CONSTRAINT `nrousuario` FOREIGN KEY (`nrousuario`) REFERENCES `usuario` (`nrousuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -164,10 +165,10 @@ DROP TABLE IF EXISTS `sala`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sala` (
-  `idSala` int unsigned NOT NULL AUTO_INCREMENT,
+  `idsala` int unsigned NOT NULL AUTO_INCREMENT,
   `capacidadmax` int unsigned NOT NULL,
-  PRIMARY KEY (`idSala`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`idsala`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,7 +177,7 @@ CREATE TABLE `sala` (
 
 LOCK TABLES `sala` WRITE;
 /*!40000 ALTER TABLE `sala` DISABLE KEYS */;
-INSERT INTO `sala` VALUES (1,30),(2,20);
+INSERT INTO `sala` VALUES (1,40),(2,45);
 /*!40000 ALTER TABLE `sala` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,6 +194,7 @@ CREATE TABLE `usuario` (
   `apellido` varchar(50) NOT NULL,
   `mail` varchar(50) NOT NULL,
   `contraseña` varchar(50) NOT NULL,
+  `Tipo` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`nrousuario`),
   UNIQUE KEY `mail_UNIQUE` (`mail`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -204,7 +206,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'0','0','lp@g.com','lucio123'),(2,'0','0','af@g.com','ale123'),(3,'0','0','admin@g.com','asd'),(4,'0','0','lb@g.com','lea123'),(5,'0','0','mc@g.com','mati123'),(6,'0','0','lg@g.com','lg123'),(7,'0','0','vt@g.com','tieppo123');
+INSERT INTO `usuario` VALUES (1,'0','0','lp@g.com','lucio123','Admin'),(2,'0','0','af@g.com','ale123','Cliente'),(3,'0','0','admin@g.com','asd','Cliente'),(4,'0','0','lb@g.com','lea123','Cliente'),(5,'0','0','mc@g.com','mati123','Cliente'),(6,'0','0','lg@g.com','lg123','Cliente'),(7,'0','0','vt@g.com','tieppo123','Cliente');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -217,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-09 20:27:19
+-- Dump completed on 2023-11-19 17:35:14
