147d42d648b385b5e1f994078a319da258fb825e
Juan Ignacio <condorijuan95@gmail.com>
2023-12-14 23:16:35 -0300

    Carga de datos a la BD

--- cinemanagement.sql
+++ cinemanagement.sql
@@ -32,6 +32,16 @@ CREATE TABLE `categoria` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Ciencia ficcion'),(2,'Comedia'),(3,'Accion'),(4,'Aventura'),(5,'Animacion'),(6,'Terror');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `entrada`
 --
 
@@ -52,6 +62,15 @@ CREATE TABLE `entrada` (
   CONSTRAINT `fk_entrada_usuario` FOREIGN KEY (`nroUsuario`) REFERENCES `usuario` (`nrousuario`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entrada`
+--
+
+LOCK TABLES `entrada` WRITE;
+/*!40000 ALTER TABLE `entrada` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entrada` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `funcion`
@@ -75,6 +94,16 @@ CREATE TABLE `funcion` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `funcion`
+--
+
+LOCK TABLES `funcion` WRITE;
+/*!40000 ALTER TABLE `funcion` DISABLE KEYS */;
+INSERT INTO `funcion` VALUES ('2024-12-24','09:00:00','10:45:00',1,1),('2024-12-24','09:00:00','10:45:00',2,2),('2024-12-24','09:00:00','10:45:00',3,3),('2024-12-24','11:00:00','12:45:00',1,3),('2024-12-24','11:00:00','12:45:00',2,1),('2024-12-24','11:00:00','12:45:00',3,2),('2024-12-24','13:00:00','14:45:00',1,2),('2024-12-24','13:00:00','14:45:00',2,3),('2024-12-24','13:00:00','14:45:00',3,1),('2024-12-25','09:00:00','10:45:00',1,1),('2024-12-25','11:00:00','12:45:00',2,2),('2024-12-25','13:00:00','14:45:00',3,3),('2024-12-26','09:00:00','10:45:00',1,3),('2024-12-26','11:00:00','12:45:00',2,2),('2024-12-26','13:00:00','14:45:00',3,1),('2024-12-27','09:00:00','10:45:00',1,1),('2024-12-27','11:00:00','12:45:00',2,2),('2024-12-27','13:00:00','14:45:00',3,3),('2024-12-28','09:00:00','10:45:00',1,2),('2024-12-28','11:00:00','12:45:00',2,1),('2024-12-28','13:00:00','14:45:00',3,3),('2024-12-29','09:00:00','10:45:00',1,2),('2024-12-29','11:00:00','12:45:00',2,3),('2024-12-29','13:00:00','14:45:00',3,1),('2025-01-01','15:00:00','17:00:00',1,4),('2025-01-01','17:30:00','19:30:00',2,5),('2025-01-01','20:00:00','22:00:00',3,6),('2025-01-02','15:00:00','17:00:00',3,4),('2025-01-02','17:30:00','19:30:00',2,6),('2025-01-02','20:00:00','22:00:00',1,5),('2025-01-03','15:00:00','17:00:00',3,6),('2025-01-03','17:30:00','19:30:00',2,5),('2025-01-03','20:00:00','22:00:00',1,4),('2025-01-04','15:00:00','17:00:00',2,5),('2025-01-04','17:30:00','19:30:00',3,6),('2025-01-04','20:00:00','22:00:00',1,4),('2025-01-05','17:30:00','19:30:00',3,5),('2025-01-05','20:00:00','22:00:00',2,6),('2025-01-05','22:00:00','17:00:00',1,4);
+/*!40000 ALTER TABLE `funcion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pelicula`
 --
 
@@ -89,8 +118,18 @@ CREATE TABLE `pelicula` (
   PRIMARY KEY (`idpelicula`),
   KEY `idcategoria_idx` (`idcategoria`),
   CONSTRAINT `idcategoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelicula`
+--
+
+LOCK TABLES `pelicula` WRITE;
+/*!40000 ALTER TABLE `pelicula` DISABLE KEYS */;
+INSERT INTO `pelicula` VALUES (1,1,'La noche de ciencia ficcion','upload\\Noche_Ciencia_ficcion.jpg'),(2,2,'Pura Comedia','upload\\Pura_Comedia.jpg'),(3,3,'Accion Turbo','upload\\accion_Turbo.jpg'),(4,4,'Ace Ventura','upload\\Ace_Ventura.jpg'),(5,5,'Animation Toon','upload\\Toon.jpg'),(6,6,'La noche de terror 1','upload\\Noche_Terror.jpg');
+/*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `reseña`
@@ -114,6 +153,16 @@ CREATE TABLE `reseña` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `reseña`
+--
+
+LOCK TABLES `reseña` WRITE;
+/*!40000 ALTER TABLE `reseña` DISABLE KEYS */;
+INSERT INTO `reseña` VALUES (1,'Es una buena pelicula','2024-12-25',1,5),(2,'No lo disfrute','2024-12-24',1,1),(3,'hola','2023-12-06',1,1);
+/*!40000 ALTER TABLE `reseña` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `sala`
 --
 
@@ -124,8 +173,18 @@ CREATE TABLE `sala` (
   `idsala` int unsigned NOT NULL AUTO_INCREMENT,
   `capacidadmax` int unsigned NOT NULL,
   PRIMARY KEY (`idsala`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sala`
+--
+
+LOCK TABLES `sala` WRITE;
+/*!40000 ALTER TABLE `sala` DISABLE KEYS */;
+INSERT INTO `sala` VALUES (1,15),(2,10),(3,15),(4,6),(5,5);
+/*!40000 ALTER TABLE `sala` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -147,8 +206,14 @@ CREATE TABLE `usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping routines for database 'cinemanagement'
+-- Dumping data for table `usuario`
 --
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'admin','admin','admin@admin','admin','Admin'),(2,'alejo','pereira','af@g.com','ale123','Cliente'),(3,'tomas','salva','admin@g.com','asd','Cliente'),(4,'matias','secchini','lb@g.com','lea123','Cliente'),(5,'tomas','soto','mc@g.com','mati123','Cliente'),(6,'luigi','mario','lg@g.com','lg123','Cliente'),(7,'violeta','martines','vt@g.com','tieppo123','Cliente'),(8,'cliente','cliente','cliente@cliente','cliente','Cliente');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -159,4 +224,4 @@ CREATE TABLE `usuario` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-14 21:04:58
+-- Dump completed on 2023-12-14 23:15:04
