38f61e1d7cf51dfb4191e60858420c50a3bcfdcb
Juan Ignacio <condorijuan95@gmail.com>
2023-12-15 19:41:36 -0300

    correcion de url para eliminar funciones

--- CineWeb/src/main/java/servlets/EliminarFuncion.java
+++ CineWeb/src/main/java/servlets/EliminarFuncion.java
@@ -15,7 +15,7 @@ import entities.*;
 /**
  * Servlet implementation class EliminarFuncion
  */
-@WebServlet("/EliminarFuncion")
+@WebServlet("/Admin/Funciones/EliminarFuncion")
 public class EliminarFuncion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
